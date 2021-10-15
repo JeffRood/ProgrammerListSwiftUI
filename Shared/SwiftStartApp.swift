b834a29dce65bfe7258fd36072a9dbeae7ecf434
Jeffry Rodriguez <jeffry.martinez@solvex.com,do>
2021-10-15 09:35:56 -0400

    Initial Commit

---
+++ Shared/SwiftStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStartApp.swift
+//  Shared
+//
+//  Created by Jeffry Rodriguez on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
